30f98fbc3366d5769595b19431b332fc3631aa24
Levi Moreira <levi.m.albuquerque@gmail.com>
2017-05-30 14:22:45 -0300

    added fonts and rich imagery to initial screen

--- app/src/main/java/br/edu/ifce/lds/coapp/LandingActivity.kt
+++ app/src/main/java/br/edu/ifce/lds/coapp/LandingActivity.kt
@@ -6,13 +6,42 @@ import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.View
+import android.widget.Toast
 
-class LandingActivity : AppCompatActivity() {
+import kotlinx.android.synthetic.main.activity_landing.*
+import org.jetbrains.anko.onClick
+
+class LandingActivity : BaseActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing)
 
+        setUpViews()
+    }
+
+    fun setUpViews() {
+
+        buttonPlans.onClick {
+            Toast.makeText(this, R.string.meet_ou_plans, Toast.LENGTH_SHORT).show()
+        }
+
+        buttonEmergencySchedule.onClick {
+            Toast.makeText(this, R.string.emergency_sheducling, Toast.LENGTH_SHORT).show()
+        }
+
+        buttonContact.onClick {
+            Toast.makeText(this, R.string.contact_us, Toast.LENGTH_SHORT).show()
+        }
+
+        buttonJoin.onClick {
+            Toast.makeText(this, R.string.sign_up, Toast.LENGTH_SHORT).show()
+        }
+
+        buttonSignIn.onClick {
+            Toast.makeText(this, R.string.sign_in, Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
