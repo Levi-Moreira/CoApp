2ac19f295ca48edbf71c6e541c0e9321e9610afb
Levi Moreira <levi.m.albuquerque@gmail.com>
2017-05-30 10:16:11 -0300

    Initial commit

---
+++ app/src/main/java/br/edu/ifce/lds/coapp/LandingActivity.kt
@@ -0,0 +1,18 @@
+package br.edu.ifce.lds.coapp
+
+import android.os.Bundle
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.View
+
+class LandingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_landing)
+
+    }
+
+}
